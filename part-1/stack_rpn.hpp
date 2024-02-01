7b3949ab175fdc1627a73f4a15bf12af2e1518aa
Chuanxia Zheng <chuanxia001@e.ntu.edu.sg>
2024-02-01 13:14:49 +0000

    Update stack_rpn.hpp

--- part-1/stack_rpn.hpp
+++ part-1/stack_rpn.hpp
@@ -20,6 +20,25 @@ void multiplies(Stack<T>& stack) {
     stack.push(a * b);
 }
 
+template <typename T>
+class StackEnhanced : public Stack<T> {
+public:
+    using Stack<T>::Stack;  // Inherit constructors
+
+    void clear() {
+        while (!this->empty()) {
+            this->pop();
+        }
+    }
+};
+
+// Overload for pushing values onto the stack
+template <typename T>
+Stack<T>& operator<<(Stack<T>& stack, const T& value) {
+    stack.push(value);
+    return stack;
+}
+
 // Overload for applying operations (like plus, multiplies)
 template <typename T>
 Stack<T>& operator<<(Stack<T>& stack, void (*func)(Stack<T>&)) {
