07945028529c3a84c1946fd254e074cd31d1bbf5
Chuanxia Zheng <chuanxia001@e.ntu.edu.sg>
2024-02-01 13:03:35 +0000

    Update stack_rpn.hpp

--- part-1/stack_rpn.hpp
+++ part-1/stack_rpn.hpp
@@ -6,5 +6,25 @@
 #include <functional>
 
 // WRITE YOUR CODE HERE
+template <typename T>
+void plus(Stack<T>& stack) {
+    T a = stack.top(); stack.pop();
+    T b = stack.top(); stack.pop();
+    stack.push(a + b);
+}
+
+template <typename T>
+void multiplies(Stack<T>& stack) {
+    T a = stack.top(); stack.pop();
+    T b = stack.top(); stack.pop();
+    stack.push(a * b);
+}
+
+// Overload for applying operations (like plus, multiplies)
+template <typename T>
+Stack<T>& operator<<(Stack<T>& stack, void (*func)(Stack<T>&)) {
+    func(stack);
+    return stack;
+}
 
 #endif // __stack_rpn__
